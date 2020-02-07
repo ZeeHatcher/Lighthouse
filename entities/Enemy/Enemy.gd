3e141856ec7f06ed4eec59287bbb984903f215af
ZeeHatcher <zee.hatcher010@gmail.com>
2020-02-07 13:42:42 +0800

    add submodule

---
+++ entities/Enemy/Enemy.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+export (int) var speed := 100
+
+var velocity := Vector2(speed, 0)
+
+func _ready() -> void:
+	add_to_group("enemies")
+
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
+
