b9be9bed0f90ceaa760fc0fd2c6bd9c064eaf7e2
Zircon332 <43201658+Zircon332@users.noreply.github.com>
2020-02-08 00:05:14 +0800

    Improved fly scripts # cleaner + documented comments

---
+++ entities/Enemy/Fly/Fly_projectile.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+onready var player = get_tree().get_root().get_node("TestLab/Player")
+
+var velocity = Vector2(speed, 0)
+
+export var speed = 500
+
+func _physics_process(delta):
+	var collision =  move_and_collide(velocity * delta)    	 # move
+	
+	if collision:         	# if collision detected #
+		queue_free()      	# destroy itself        #
+
+# destroy upon exiting screen
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+# set direction
+func point():
+	look_at(player.global_position)
+	velocity = velocity.rotated(rotation + rand_range(-0.1, 0.1))
