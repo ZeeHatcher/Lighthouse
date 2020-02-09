9bbd1205af7302b81dd7071042c916f54273a8b1
hypermagi <shamil.haqeem@gmail.com>
2020-02-09 11:26:47 +0800

    Added bg music and stair sound

--- levels/Camera2D.gd
+++ levels/Camera2D.gd
@@ -1,18 +1,7 @@
 extends Camera2D
 
-export var zoom_size = (Vector2(2,2))
+export var zoom_size = Vector2(1.5,1.5)
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_zoom(zoom_size)
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
