bcfb1a52bd3707927159fd0a7c9a47027a905137
Zircon332 <43201658+Zircon332@users.noreply.github.com>
2020-02-10 15:11:56 +0800

    lightning

--- entities/Gun/Gun.gd
+++ entities/Gun/Gun.gd
@@ -10,3 +10,14 @@ func fire() -> void:
 	
 	projectile.global_position = muzzle.global_position
 	projectile.point_to(get_global_mouse_position())
+
+#
+#onready var scene_lightning = preload("res://assets/Lightning.tscn")
+#func fire() -> void:
+#	var lightning : Line2D = scene_lightning.instance()
+#	level.add_child(lightning)
+#
+#	lightning.global_position = muzzle.global_position
+#	lightning.set_point_position(1, get_global_mouse_position() - muzzle.global_position)
+#	lightning.area.position = get_global_mouse_position() - muzzle.global_position
+
