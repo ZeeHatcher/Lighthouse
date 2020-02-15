002d9307b9dfc2060e025b8cbafac785bccbbef1
Zircon332 <43201658+Zircon332@users.noreply.github.com>
2020-02-15 11:45:13 +0800

    Improved camera script + added minimum value before camera starts shifting to mouse

--- levels/Camera2D.gd
+++ levels/Camera2D.gd
@@ -1,4 +1,10 @@
 extends Camera2D
-	
+
+export (int) var min_camera_limit = 200
+
 func _process(delta: float):
-	position.x = get_local_mouse_position().x
+	if abs(get_local_mouse_position().x) > min_camera_limit:
+		if get_local_mouse_position().x > 0:
+			position.x = get_local_mouse_position().x - min_camera_limit
+		else:
+			position.x = get_local_mouse_position().x + min_camera_limit
