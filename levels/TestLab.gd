dbb81ee78e127e3a7478d75a284b4661ed16644d
hypermagi <shamil.haqeem@gmail.com>
2020-06-24 14:19:50 +0800

    Added quit game on escape key

--- levels/TestLab.gd
+++ levels/TestLab.gd
@@ -17,5 +17,7 @@ func _process(_delta):
 	var enemy = get_tree().get_nodes_in_group("enemies").size();
 	if  enemy != null:
 		enemy_text = enemy
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
 		
 	OS.set_window_title(title + " | fps: " + str(Engine.get_frames_per_second()) + " | Mob count: " + str(enemy_text))
