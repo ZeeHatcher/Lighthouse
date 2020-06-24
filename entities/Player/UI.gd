2b1d6bd48236318a8426da724f282d211d79b56a
hypermagi <shamil.haqeem@gmail.com>
2020-06-24 17:06:14 +0800

    Fixed focus issue with line edit boxes.

--- entities/Player/UI.gd
+++ entities/Player/UI.gd
@@ -13,3 +13,8 @@ func _ready():
 
 func _on_Gun_fired():
 	$vbox/LineEdit2.text = str(get_node("../../Gun").ammo)
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_cancel"):
+		for i in $vbox.get_children():
+			i.release_focus()
